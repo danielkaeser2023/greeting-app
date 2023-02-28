156934387e2acf7bf614b8b0fc3276dabba8ed87
daniel.kaeser <external.daniel.kaeser@gmail.com>
2023-02-28 16:44:51 +0100

    add k8s resources

--- src/main/java/com/example/greetingapp/GreetingAppApplication.java
+++ src/main/java/com/example/greetingapp/GreetingAppApplication.java
@@ -1,7 +1,14 @@
 package com.example.greetingapp;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GreetingAppApplication {
@@ -9,5 +16,24 @@ public class GreetingAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingAppApplication.class, args);
 	}
+}
 
+@RestController
+@RequiredArgsConstructor
+class GreetingController {
+
+	private final GreetingConfigurationProperties properties;
+
+	@GetMapping("/greeting")
+	public String greeting() {
+		return properties.getGreeting();
+	}
+}
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("some")
+class GreetingConfigurationProperties {
+	private String greeting;
 }
